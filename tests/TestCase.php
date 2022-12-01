4112ac5cfaa6cbea46c3a4ca3df39b26692bacca
andreas_kraenzle <kraenzle@k-r.ch>
2022-12-01 09:57:22 +0100

    Remove Laravel dependency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,4 @@
 namespace KraenzleRitter\MarkdownToEad\Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
-{
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            'KraenzleRitter\MarkdownToEad\MarkdownToEadServiceProvider',
-        ];
-    }
-}
+{}
