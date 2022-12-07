64c3b67c7ecd3ccc061c3ea7638c3a1dd5b05d57
andreas_kraenzle <kraenzle@k-r.ch>
2022-12-07 16:46:35 +0100

    Update MarkdownToEadTest.php

--- tests/MarkdownToEadTest.php
+++ tests/MarkdownToEadTest.php
@@ -9,12 +9,11 @@ class MarkdownToEadTest extends TestCase
 {
     public function test_simple()
     {
-\Log::debug('hallo');
         $markdown = '# Hello World!';
 
         $converter = new Convert();
         $actual = trim($converter->toEad($markdown));
-        $expected = '<head>Hello World!</head>';
+        $expected = '<p>Hello World!</p>';
         $this->assertEquals(trim((string) $expected), (string) $actual);
     }
 
@@ -57,7 +56,7 @@ Inline [Link](url).
 ';
         $converter = new Convert();
         $actual = $converter->toEad($markdown);
-        $expected = '<head>Überschrift</head><p>Paragraph <emph render="italic">kursiv</emph> <emph render="bold">fett</emph></p><list><item>Item 1</item><item>Item 2</item></list><p>Inline <extref xlink:href="url">Link</extref>.</p>';
+        $expected = '<p>Überschrift</p><p>Paragraph <emph render="italic">kursiv</emph> <emph render="bold">fett</emph></p><list><item>Item 1</item><item>Item 2</item></list><p>Inline <extref xlink:href="url">Link</extref>.</p>';
         $this->assertEquals($expected, $actual);
     }
 }
